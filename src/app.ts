cc05af680a652330581ca85d823aa4c5d9ae1575
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-04-17 11:38:13 +0530

    feat: add morgan for logging and implement global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import env from './config/env';
+import morgan from 'morgan';
+import { globalErrorHandler } from './middlewares/error-handler.middleware';
 
 const app = express();
+
+const isProduction = env.NODE_ENV === 'PRODUCTION';
 
 app.use(
     cors({
@@ -11,17 +15,28 @@ app.use(
         credentials: true,
     })
 );
+app.use(morgan(isProduction ? 'combined' : 'dev'));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/healthcheck', (_, res) => {
+app.get('/', (_: Request, res: Response) => {
     res.status(200).send('OK').json({
         success: true,
         message: 'Server is running',
         data: null,
     });
 });
+
+app.use('*', (req: Request, res: Response) => {
+    res.status(404).send('Not Found').json({
+        success: false,
+        message: 'Not Found',
+        data: null,
+    });
+});
+
+app.use(globalErrorHandler);
 
 export { app };
---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,10 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(500).json({
+        success: false,
+        message: 'Internal Server Error',
+        data: null,
+    });
+};

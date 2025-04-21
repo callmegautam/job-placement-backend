5d63e595d357e67cde93f01a6d0a3aaea9afcfa9
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-04-21 11:42:42 +0530

    fix(jwt): correct constant name for token expiration feat(types): add job type, job mode, and skill types; export corresponding options

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,7 +2,7 @@ import env from '@/config/env';
 import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = env.JWT_SECRET;
-const JWT_EXPIRE_IN = env.JWT_EXPIRY;
+const JWT_EXPIRES_IN = env.JWT_EXPIRY;
 
 export function generateToken(payload: string | Buffer | Object): string {
     // @ts-expect-error
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,11 @@
-import { course } from '@/db/schema';
+import { course, jobType, jobMode, skillsEnum } from '@/db/schema';
 
 export type CourseType = (typeof course.enumValues)[number];
+export type JobType = (typeof jobType.enumValues)[number];
+export type JobMode = (typeof jobMode.enumValues)[number];
+export type Skill = (typeof skillsEnum.enumValues)[number];
+
+export const COURSE_OPTIONS = course.enumValues;
+export const JOB_TYPE_OPTIONS = jobType.enumValues;
+export const JOB_MODE_OPTIONS = jobMode.enumValues;
+export const SKILL_OPTIONS = skillsEnum.enumValues;
